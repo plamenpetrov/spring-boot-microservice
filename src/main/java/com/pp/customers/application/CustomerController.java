7c367e5b0a96289da1ee21f70f5cac63072ddb38
Plamen <plamen.petrov@takeaway.com>
2020-06-18 14:28:55 +0300

    Add method description

--- src/main/java/com/pp/customers/application/CustomerController.java
+++ src/main/java/com/pp/customers/application/CustomerController.java
@@ -16,28 +16,50 @@ public class CustomerController {
         this.service = service;
     }
 
-    // Aggregate root
+    /**
+     * Aggregate root. Return all customers
+     * @return
+     */
     @GetMapping("/customers")
     List<Customer> all() {
         return service.findAll();
     }
 
+    /**
+     * Create new customer
+     * @param newCustomer
+     * @return
+     */
     @PostMapping("/customers")
     Customer newCustomer(@RequestBody Customer newCustomer) {
         return service.save(newCustomer);
     }
 
-    // Single item
+    /**
+     * Get information for single customer
+     * @param id
+     * @return
+     */
     @GetMapping("/customers/{id}")
     Customer one(@PathVariable Long id) {
         return service.find(id);
     }
 
+    /**
+     * Create new customer based on id of already existing one
+     * @param newCustomer
+     * @param id
+     * @return
+     */
     @PutMapping("/customers/{id}")
     Customer replaceCustomer(@RequestBody Customer newCustomer, @PathVariable Long id) {
         return service.updateCustomer(newCustomer, id);
     }
 
+    /**
+     * Delete customer
+     * @param id
+     */
     @DeleteMapping("/customers/{id}")
     void deleteCustomer(@PathVariable Long id) {
         service.deleteById(id);
